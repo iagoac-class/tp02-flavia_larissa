0d53fa9fdc4fa21c1400ee2c1d9910ac9276afc2
Larissa Ávila <larissa.avila@sou.unifal-mg.edu.br>
2024-10-11 17:13:18 -0300

    funcoes avl

--- arvores.c
+++ arvores.c
@@ -6,13 +6,8 @@
 
 #define CAMINHO_FIXO "instancias/"
 
-typedef struct no_
-{
-    int valor;
-    struct no_ *esq;
-    struct no_ *dir;
-} no;
 
+//Funções utilizadas na árvore binária simples
 no * aloca_no(int valor)
 {
     no *novo = (no *)malloc(sizeof(no));
@@ -200,6 +195,120 @@ double arvore_binaria(int instancia_num) {
     return (tempo);
 }
 
+//Funções utilizadas na árvore balanceada
+// Obtem a altura da AVL
+int altura(struct Node *N) { 
+    if (N == NULL) {
+        return 0; 
+    }
+    return N->altura; 
+} 
+  
+// Obtem o máximo entre dois inteiros
+int max(int a, int b)  { 
+    if (a < b) {
+        return (b);
+    } 
+    return (a); 
+} 
+  
+/* Cria um novo nó com apontadores esquerda e direita nulos */
+struct Node* newNode(int valor) { 
+    struct Node* node = (struct Node*) malloc(sizeof(struct Node)); 
+    node->valor     = valor; 
+    node->esquerda  = NULL; 
+    node->direita   = NULL; 
+    node->altura    = 1;  // Novo nó é adicionado como folha
+    return(node); 
+} 
+  
+// Faz a rotação a direita na raiz y
+struct Node *direitaRotate(struct Node *y) { 
+    struct Node *x = y->esquerda; 
+    struct Node *T2 = x->direita; 
+  
+    // Realiza a rotação
+    x->direita = y; 
+    y->esquerda = T2; 
+  
+    // Atualiza as alturas
+    y->altura = max(altura(y->esquerda), altura(y->direita)) + 1; 
+    x->altura = max(altura(x->esquerda), altura(x->direita)) + 1; 
+  
+    // Retorna a nova raiz
+    return x; 
+} 
+  
+// Faz a rotação a esquerda na raiz x
+struct Node *esquerdaRotate(struct Node *x) { 
+    struct Node *y = x->direita; 
+    struct Node *T2 = y->esquerda;
+
+    y->esquerda = x;
+    x->direita = T2;
+
+    x->altura = max(altura(x->esquerda), altura(x->direita)) + 1;
+    y->altura = max(altura(y->esquerda), altura(y->direita)) + 1;
+    
+    // Retorna a nova raiz
+    return y; 
+} 
+  
+// Obtem o fator de balanceamento para o nó N
+int balanceamento(struct Node *N) { 
+    if (N == NULL) {
+        return 0; 
+    }
+    return (altura(N->esquerda) - altura(N->direita)); 
+} 
+
+//Faz a inserção do nó na árvore (função recursiva)
+struct Node* inserir(struct Node* node, int valor) { 
+    /* 1.  Realiza a inserção normal de árvore binária */
+    if (node == NULL) {
+        return(newNode(valor)); 
+    }
+  
+    if (valor < node->valor) {
+        node->esquerda  = inserir(node->esquerda, valor); 
+    } else if (valor > node->valor){ 
+        node->direita = inserir(node->direita, valor); 
+    } else { 
+        // Valores duplicados não são permitidos na AVL
+        return node; 
+    }
+    /* 2. Atualiza a altura de seu antecessor */
+    node->altura = 1 + max(altura(node->esquerda), 
+                        altura(node->direita)); 
+  
+    /* 3. Obtem o fator de balanceamento da raiz */
+    int balance = balanceamento(node); 
+
+    // Se a árvore está desbalanceada, então existem quatro casos possíveis
+  
+    // Caso esquerda esquerda
+    if (balance > 1 && valor < node->esquerda->valor) {
+        return direitaRotate(node); 
+    }
+    // Caso direita direita
+    if (balance < -1 && valor > node->direita->valor) {
+        return esquerdaRotate(node); 
+    }
+    //Caso esquerda direita
+    if (balance > 1 && valor > node->esquerda->valor) { 
+        node->esquerda =  esquerdaRotate(node->esquerda); 
+        return direitaRotate(node); 
+    } 
+    //Caso direita esquerda
+    if (balance < -1 && valor < node->direita->valor) { 
+        node->direita = direitaRotate(node->direita); 
+        return esquerdaRotate(node); 
+    } 
+
+    //retorna o ponteiro para a raiz
+    return node;
+}
+  
 double arvore_balanceada(int instancia_num) {
     double tempo = 0;
     clock_t begin = clock();
--- arvores.h
+++ arvores.h
@@ -2,6 +2,19 @@
 #define _H_ARVORES
 
 // Implementar qualquer struct que for necessária aqui
+typedef struct no_
+{
+    int valor;
+    struct no_ *esq;
+    struct no_ *dir;
+} no;
+
+struct Node {
+    int valor;
+    struct Node *esquerda;
+    struct Node *direita;
+    int altura;
+};
 
 double arvore_binaria(int instancia_num);
 double arvore_balanceada(int instancia_num);
